672d192905691bc208756d9364d5ec882c898021
Jayavignesh P <jayavignesh.p2019@vitstudent.ac.in>
2021-11-16 18:09:05 +0530

    Updated

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,6 +4,12 @@ import "../styles/movie-card.css"
 import MovieDet from './MovieDet'
 
 export default function MovieCard({movie,movie_id,setMovie_id,getMovieDet,movie_det}) {
+    function changeBack(e) {
+        e.target.style.background = 'red';
+    }
+    function Original(e) {
+        e.target.style.background = 'white';
+    }
     return (
         <a
             onClick={() => {
@@ -13,9 +19,10 @@ export default function MovieCard({movie,movie_id,setMovie_id,getMovieDet,movie_
             }}    
         >
             <div className="movie-card">
-                <img src={movie.Poster} alt={movie.Title} />
-                <p>{movie.Title}</p>
-                <p>{movie.Year}</p>
+                <img className="Image" onMouseOver={changeBack} onMouseLeave={Original} src={movie.Poster} alt={movie.Title} />
+                <br />
+                <p className="Alli">{movie.Title}</p>
+                <p className="Alli">{movie.Year}</p>
                 {movie_id === movie.imdbID && <MovieDet movie_det={movie_det} />}
             </div>
         </a>
--- src/components/MovieDet.js
+++ src/components/MovieDet.js
@@ -4,10 +4,10 @@ import "../styles/movie-det.css"
 export default function ({movie_det}) {
     return (
         <div className="movie-det">
-            <pre><p className="Title">Title     :-  {movie_det.Title}</p>
+            <p className="Title">Title     :-  {movie_det.Title}</p>
             <p>Runtime   :-  {movie_det.Runtime}</p>
             <p>Genre     :-  {movie_det.Genre}</p>
-            <p>Director  :-  {movie_det.Director}</p></pre>
+            <p>Director  :-  {movie_det.Director}</p>
         </div>
     )
 }
